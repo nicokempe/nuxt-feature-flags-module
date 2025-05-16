e090edbb1bdde9b65544e373ac764cce3e459505
Nico Kempe <git@nicokempe.de>
2025-05-16 22:52:22 +0200

    feat(flags): Create defineFeatureFlagMiddleware

---
+++ src/runtime/middleware/defineFeatureFlagMiddleware.ts
@@ -0,0 +1,10 @@
+import { useFeatureFlag } from '~/src/runtime/composables/useFeatureFlag'
+
+export const defineFeatureFlagMiddleware = (flag: string) => {
+  return defineNuxtRouteMiddleware(() => {
+    const { isEnabled } = useFeatureFlag()
+    if (!isEnabled(flag)) {
+      return showError({ statusCode: 404, statusMessage: 'Feature not available' })
+    }
+  })
+}
