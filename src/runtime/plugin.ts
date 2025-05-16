766369e37ce9a0f106f291c9edaeeebf561d89a7
Nico Kempe <git@nicokempe.de>
2025-05-16 22:52:56 +0200

    feat(flags): Update plugin types and load feature directive

--- src/runtime/plugin.ts
+++ src/runtime/plugin.ts
@@ -1,5 +1,7 @@
+import { vFeature } from './directives/v-feature'
 import { defineNuxtPlugin } from '#app'
 
-export default defineNuxtPlugin((_nuxtApp) => {
+export default defineNuxtPlugin((_nuxtApp): void => {
   console.log('Plugin injected by nuxt-feature-flags-module!')
+  _nuxtApp.vueApp.directive('feature', vFeature)
 })
