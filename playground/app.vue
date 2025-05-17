509f23ec79abf13484d0dc92490c8ccdca9d710a
Nico Kempe <git@nicokempe.de>
2025-05-17 15:12:59 +0200

    feat(app): Update template to use NuxtPage and add Tailwind CSS script

--- playground/app.vue
+++ playground/app.vue
@@ -1,8 +1,14 @@
 <template>
   <div>
-    Nuxt module playground!
+    <NuxtPage />
   </div>
 </template>
 
-<script setup>
+<script setup lang="ts">
+onMounted(() => {
+  const script = document.createElement('script')
+  script.src = 'https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4'
+  script.defer = true
+  document.head.appendChild(script)
+})
 </script>
