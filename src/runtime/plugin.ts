f534a00af5d39094afd2d95b4a92b8cea0e16dcd
Nico Kempe <git@nicokempe.de>
2025-05-18 19:57:14 +0200

    chore(plugin): remove console log from feature flags plugin

--- src/runtime/plugin.ts
+++ src/runtime/plugin.ts
@@ -2,6 +2,5 @@ import { vFeature } from './directives/v-feature'
 import { defineNuxtPlugin } from '#app'
 
 export default defineNuxtPlugin((_nuxtApp): void => {
-  console.log('Plugin injected by nuxt-feature-flags-module!')
   _nuxtApp.vueApp.directive('feature', vFeature)
 })
