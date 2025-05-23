6adf62a0571055589b749436aa7e8fecf1cb4628
Nico Kempe <git@nicokempe.de>
2025-05-23 21:47:17 +0200

    feat(playground): enhance layout and styling for feature flag protection page message

--- playground/pages/protected.vue
+++ playground/pages/protected.vue
@@ -1,6 +1,12 @@
 <template>
-  <div>
-    <h2>This page is protected by feature flag: <code>feature-new-system</code></h2>
+  <div class="min-h-screen p-12 text-white bg-neutral-950">
+    <h2 class="text-2xl font-bold mb-4">
+      🔐 Protected Feature Page
+    </h2>
+    <p>
+      This page is protected by feature flag:
+      <code class="text-emerald-400">newSystem</code>
+    </p>
   </div>
 </template>
 
