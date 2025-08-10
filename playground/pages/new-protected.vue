dfdd15789f65d4bda952e84f7d4cfa5239c9c224
Nico Kempe <git@nicokempe.de>
2025-08-10 10:42:58 +0200

    feat(playground): add playground example for new page protection
    Signed-off-by: Nico Kempe <git@nicokempe.de>

---
+++ playground/pages/new-protected.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="min-h-screen p-12 text-white bg-neutral-950">
+    <h2 class="text-2xl font-bold mb-4">
+      🔐 Protected Feature Page
+    </h2>
+    <p>
+      This page is protected by feature flag:
+      <code class="text-emerald-400">newSystem</code>
+    </p>
+  </div>
+</template>
+
+<script setup lang="ts">
+definePageMeta({
+  featureFlag: 'newSystem',
+  featureFallback: '/404',
+  featureNotifyOnBlock: true,
+})
+</script>
